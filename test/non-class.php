28fdf12a77a388c132b64cd190de70cbce3030f6
mackenza <a.mackenzie@gmail.com>
2014-09-12 18:57:57 -0400

    more progress

--- test/non-class.php
+++ test/non-class.php
@@ -11,12 +11,12 @@ const CONSTCONST1 = "string 1";
 const CONSTCONST2_1 = 21,
       CONSTCONST2_2 = 22;
 
-function function1()
+function function1($param1 = 0)
 {
     echo "foo";
 }
 
-function function2($param1, $param2)
+function function2(&$param1, $param2 = "default value param2")
 {
     echo "bar";
 }
