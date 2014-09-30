46512f75c5a7fc7ecda6b47e5015ce59eee54f26
Andrew MacKenzie <a.mackenzie@gmail.com>
2014-09-30 12:47:27 -0400

    cleanup

--- test/psr2-ish.php
+++ test/psr2-ish.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Foo\Bar;
-
+$foo = new PDOStatement();
 class Baz
 {
     const CONST1 = "blah";
@@ -19,11 +19,15 @@ class Baz
 
     public function pubFunc1($param1 = 0, array $param2array)
     {
+        $bar = new SessionHandler;
         return null;
+        $foo2 = new Directory;
     }
 
     public function protFunc2($param1 = "param 1 string", array $param2array, &$paramByRef, Foo\Bar\Baz $paramTypeHint)
     {
+        $baz = new PDOStatement;
         return null;
+        $aaa = $foo2path
     }
 }
